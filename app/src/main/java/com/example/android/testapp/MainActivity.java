afc10b36c6113ea044d01667bfeb7febbecc0946
Igor <idmitrievnz@gmail.com>
2021-09-02 20:30:02 +1200

    Added intent for a calculator app relocation

--- app/src/main/java/com/example/android/testapp/MainActivity.java
+++ app/src/main/java/com/example/android/testapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.android.testapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.calculate_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("scheme://calculator");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(Intent.createChooser(intent, "Choose something"));
+            }
+        });
     }
 }
